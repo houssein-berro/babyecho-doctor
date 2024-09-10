58780725ddcd103082b2171eced459d5d1d1336a
Berro-1 <hussienberro1@gmail.com>
2024-09-10 17:34:19 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Users from './pages/users/users';
+import BabyAnalysis from './pages/baby/baby';
+import './App.css'
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <div className='App'>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/baby/:babyId/analysis" element={<BabyAnalysis />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </Router>
+    </div>);
 }
 
 export default App;
