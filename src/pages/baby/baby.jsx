545760ef4c75aa49c0b2d4d976c0886e9ce54691
Berro-1 <hussienberro1@gmail.com>
2024-09-15 15:51:59 +0300

    adjusted baby page layout

--- src/pages/baby/baby.jsx
+++ src/pages/baby/baby.jsx
@@ -1,36 +1,58 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchRecordingsByBaby } from '../../redux/recordings/recordingActions';
-import BabyAnalysisChart from './chart';
-import './baby.css';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchRecordingsByBaby } from "../../redux/recordings/recordingActions";
+import BabyAnalysisChart from "./chart";
+import Sidebar from "../../components/sidebar";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"; // Icon for the menu
+import "./baby.css";
+
 const BabyAnalysis = () => {
-    const { babyId } = useParams(); 
-    const dispatch = useDispatch();
-  
-    const { recordings, loading, error } = useSelector((state) => state.recordings);
-  
-    useEffect(() => {
-      dispatch(fetchRecordingsByBaby(babyId));
-    }, [babyId, dispatch]);
-  
-    if (loading) {
-      return <div className="loading">Loading...</div>;
-    }
-  
-    if (error) {
-      return <div className="error">{error}</div>;
-    }
-  
-    if (!recordings || recordings.length === 0) {
-      return <div className="no-recordings">No recordings found.</div>;
-    }
-    
-    return (
+  const { babyId } = useParams();
+  const dispatch = useDispatch();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const { recordings, loading, error } = useSelector(
+    (state) => state.recordings
+  );
+  const toggleDrawer = () => setDrawerOpen(!drawerOpen);
+
+  useEffect(() => {
+    dispatch(fetchRecordingsByBaby(babyId));
+  }, [babyId, dispatch]);
+
+  if (loading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!recordings || recordings.length === 0) {
+    return <div className="no-recordings">No recordings found.</div>;
+  }
+
+  return (
+    <div>
+      <Sidebar open={drawerOpen} onClose={toggleDrawer} />
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={toggleDrawer}
+        className="menu-button"
+      >
+        <MenuIcon />
+      </IconButton>
       <div className="baby-analysis-container">
-        {!loading && recordings.length > 0 && <BabyAnalysisChart recordings={recordings} />}
+        {!loading && recordings.length > 0 && (
+          <BabyAnalysisChart recordings={recordings} />
+        )}
       </div>
-    );
-  };
-  
-  export default BabyAnalysis;
+    </div>
+  );
+};
+
+export default BabyAnalysis;
