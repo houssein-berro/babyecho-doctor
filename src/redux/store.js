af917059eec15c02bb10ce93689c33fa22b30820
Berro-1 <hussienberro1@gmail.com>
2024-09-15 15:53:12 +0300

    Added reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './users/userSlice'; 
 import recordingsReducer from './recordings/recordingSlice';
-
+import authReducer from './auth/authSlice'
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     user: userReducer,
     recordings: recordingsReducer, 
   },
